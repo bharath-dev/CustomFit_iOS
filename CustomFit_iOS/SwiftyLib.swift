8b19f274ef523563266973cc51637f52485bf91e
Bharath R <bharath@squareoffnow.com>
2019-10-02 02:16:28 +0530

    Added test cases

---
+++ CustomFit_iOS/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  CustomFit_iOS
+//
+//  Created by Bharath R on 02/10/19.
+//  Copyright © 2019 CustomFit. All rights reserved.
+//
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
